d40b6bad6c7e694f97cf5d1fd3cfbd73c0d9e827
Anshumaan Sai <anshumaan.2k6@gmail.com>
2026-09-03 05:35:32 +0530

    Added database configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,28 @@
+import "dotenv/config";
+
 import { app } from "./app.js";
+import { connectDB } from "./config/db.js";
+import {routes} from "./routes"
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening to Port: ${PORT}`)
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, ()=> {
+      console.log(`Listening to Port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server");
+    process.exit(1);
+  }
+};
+
+startServer();
 
 app.get('/', (req, res)=> {
-  res.send("Hi");
-})
+  res.send("Hi👋");
+});
+
+app.use('/', routes);
