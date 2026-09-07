451b5413684f5291b846988ab811e8bd79fa5865
Anshumaan Sai <anshumaan.2k6@gmail.com>
2026-09-07 18:55:58 +0530

    Added initial navbar structure

---
+++ frontend/public/src/Hero/!main.jsx
@@ -0,0 +1,13 @@
+import './!main.css'
+
+import AppInfo from "./app-info";
+
+function Hero() {
+  return ( 
+    <div className="hero">
+      <AppInfo />
+    </div>
+   );
+}
+
+export default Hero;
