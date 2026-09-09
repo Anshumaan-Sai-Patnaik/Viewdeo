6b4c3934972783e234c68d996956b16642d3d48b
Anshumaan Sai <anshumaan.2k6@gmail.com>
2026-09-09 06:23:42 +0530

    Added hero layout with illustration and app description

--- frontend/public/src/Hero/!main.jsx
+++ frontend/public/src/Hero/!main.jsx
@@ -1,11 +1,13 @@
 import './!main.css'
 
 import AppInfo from "./app-info";
+import MeetIllustration from "./meet-illustration";
 
 function Hero() {
   return ( 
     <div className="hero">
       <AppInfo />
+      <MeetIllustration />
     </div>
    );
 }
---
+++ frontend/public/src/Hero/meet-illustration.jsx
@@ -0,0 +1,11 @@
+import twoPeopleInMeet from '../assets/two_people_in_meet.svg'
+
+function MeetIllustration() {
+  return ( 
+    <div className="meet-illustration">
+      <img src={twoPeopleInMeet} alt="Two people talking in a video meeting" />
+    </div>
+   );
+}
+
+export default MeetIllustration;
